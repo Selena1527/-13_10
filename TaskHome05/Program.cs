b61a1b29614c62bbffaa6274bdb3d327ff8fc0e8
Elena <elena.demeneva.90@mail.ru>
2022-10-14 15:12:17 +0500

    TaskHome05

---
+++ TaskHome05/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два чила
+// Выдает, какое число больше, какое меньше
+// a = 5, b = 7 -> max = 7
+// a = 2, b = 10 -> max = 10
+// a = -9, b = -3 -> max = -3
+
+Console.WriteLine("Введите два числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    int max = num1;
+    Console.WriteLine($"max = {num1}");
+}
+else Console.WriteLine($"max = {num2}");
