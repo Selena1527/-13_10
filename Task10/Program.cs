74fc57912a7ee38c31e46fb4706c7e34072999c4
Elena <elena.demeneva.90@mail.ru>
2022-10-18 21:19:38 +0500

    Add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100 - 999 => {number}");
+int Digit = number / 10;
+// Console.WriteLine($"{firstDigit}");
+int result = Digit % 10;
+Console.WriteLine($"{result}");
+
+
