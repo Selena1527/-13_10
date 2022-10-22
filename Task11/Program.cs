d4bbebb30b5910f65e4ae8f464c6e1505dfbf722
Elena <elena.demeneva.90@mail.ru>
2022-10-22 15:12:26 +0500

    Add task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -5,11 +5,21 @@
 // 918 -> 98
 
 int number = new Random().Next(100, 1000);
-Console.WriteLine($"Случайное тркхзначное число {number}");
+Console.WriteLine($"Случайное трехзначное число {number}");
 
-int firstDigit = number / 100;
-int thirdDigit = number % 10;
-int result = (firstDigit * 10) + thirdDigit;
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int result = (firstDigit * 10) + thirdDigit;
 
+// Console.WriteLine($"Первая и последняя цифра числа {result}");
 
+// МЕТОД
+
+int DelSecondDigit(int num)
+{
+    int firstDigit = number / 100;
+    int thirdDigit = number % 10;
+    return (firstDigit * 10) + thirdDigit;
+}
+int result = DelSecondDigit(number);
 Console.WriteLine($"Первая и последняя цифра числа {result}");
