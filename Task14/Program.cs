06f9bcade4e8b27d67ec935d98f87f630dcf6565
Elena <elena.demeneva.90@mail.ru>
2022-10-22 17:12:15 +0500

    add Task14

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -4,23 +4,25 @@
 // 14 -> нет 
 // 46 -> нет 
 // 161 -> да 
-bool DivNum(int digit, int num1, int num2) 
-{ 
-    int result1 = digit%num1; int result2 = digit%num2; 
-    if(result1==0 && result2==0) return true; 
-    return false; 
-} 
-Console.Write("Введите число: "); 
-int num = Convert.ToInt32(Console.ReadLine()); 
-int number1 = 7; 
-int number2 = 23; 
 
-bool result = DivNum(num, number1, number2); 
-if (result) 
-{ 
-    Console.WriteLine("Кратное.");
-} 
-else 
-{ 
-    Console.WriteLine("Не кратны."); 
+bool DivNum(int digit, int num1, int num2)
+{
+    int result1 = digit % num1;
+    int result2 = digit % num2;
+    if(result1==0 && result2==0) return true;
+    return false;
+}
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int number1 = 7;
+int number2 = 23;
+
+bool result = DivNum(num, number1, number2);
+if (result)
+{
+    Console.WriteLine("Кратное");
+}
+else
+{
+    Console.WriteLine("Не кратное.");
 }
