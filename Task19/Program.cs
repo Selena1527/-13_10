14ebce85b9fdbafeb6eda0978f8acd674245610e
Elena <elena.demeneva.90@mail.ru>
2022-11-11 19:36:33 +0500

    исправила ошибки и дополнила проверкой

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -10,27 +10,30 @@
 Console.WriteLine("Введите пятизначное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-int A1 = n / 10000;
-int A2 = n % 10;
-int B1 = (n / 1000) % 10;
-int B2 = (n % 10) % 10;
 
-bool Revers(int N, int A1, int A2, int B1, int B2)
+bool Revers(int N)
 {
     int a1 = N / 10000;
     int a2 = N % 10;
     int b1 = (N / 1000) % 10;
-    int b2 = (N /10) % 10;
-    if(a1 == a2 && b1 == b2) return true;
+    int b2 = (N / 10) % 10;
+    if (a1 == a2 && b1 == b2) return true;
     return false;
 }
 
-bool result = Revers(n, A1, A2, B1, B2);
-if (result)
+if (n >= 10000 && n <= 99999)
 {
-    Console.WriteLine("Да, является палиндромом.");
+    bool result = Revers(n);
+    if (result)
+    {
+        Console.WriteLine("Да, является палиндромом.");
+    }
+    else
+    {
+        Console.WriteLine("Нет, не является палиндромом.");
+    }
 }
 else
 {
-    Console.WriteLine("Нет, не является палиндромом.");
+    Console.WriteLine("Введите пятизначное число");
 }
