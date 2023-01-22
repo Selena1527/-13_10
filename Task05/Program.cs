73201242ac8c3f62fad19805b473b8f841c12c8e
Savina Elena <elena.demeneva.90@mail.ru>
2023-01-23 01:33:07 +0500

    formatting

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -8,11 +8,12 @@ Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 if (number > 0)
-{int count = -number;
-while (count <= number)
 {
-    Console.Write($"{count} ");
-    count++;  //count = count +1
-}
+    int count = -number;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count++;  //count = count +1
+    }
 }
 else Console.WriteLine("Введите целое положительное число!");
